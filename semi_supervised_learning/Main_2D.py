ee60ba0487cb976fac1df02e5cbee51b71b27eb8
Juul Van Boxtel <62057578+Juulvb@users.noreply.github.com>
2021-03-17 12:57:26 +0100

    added ipython test file, fixed validation bug

--- semi_supervised_learning/Main_2D.py
+++ semi_supervised_learning/Main_2D.py
@@ -7,15 +7,13 @@ from Data_2D import load_data, print_func, save_results, elastic_deformation
 from sklearn.model_selection import KFold
 from tensorflow.keras import backend as K
 
-from tensorflow.keras.layers import BatchNormalization#, LayerNormalization
-# from tensorflow_addons.layers import InstanceNormalization, GroupNormalization
+from tensorflow.keras.layers import BatchNormalization, LayerNormalization
+#from tensorflow_addons.layers import InstanceNormalization, GroupNormalization
 
 K.set_image_data_format('channels_last')  # TF dimension ordering in this code
 
 # data_path = r"C:\Users\20164798\OneDrive - TU Eindhoven\UNI\ME 1\Q3\CS of MI\Image Registration Project\Data_ML\train"
 # save_path = r""
-data_path = r"/home/8dm20-7/Data/train"
-save_path = r"/home/8dm20-7/Results"
 
 train_data_gen_args = dict(rotation_range=90, width_shift_range=0.1, height_shift_range=0.1,
                            shear_range=0.2, zoom_range=0.2, horizontal_flip=True, 
@@ -23,13 +21,13 @@ train_data_gen_args = dict(rotation_range=90, width_shift_range=0.1, height_shif
 
 
 
-def train_model(data_path=data_path, imgs="mr_bffe.mhd", msks="prostaat.mhd", model_name="model", save_path = "results", x_size = 320, y_size = 256, num_folds=5, batch_size=32, learning_rate=1e-5, nr_epochs=80, verbosity=1, up=False, start_ch=32, depth=4, inc_rate=2, kernel_size=(3, 3), activation='relu', normalization=None, dropout=0.2, augment_factor=5):
+def train_model(data_path, imgs="mr_bffe.mhd", msks="prostaat.mhd", model_name="model", save_path = "results", x_size = 320, y_size = 256, num_folds=5, batch_size=32, learning_rate=1e-5, nr_epochs=80, verbosity=1, up=False, start_ch=32, depth=4, inc_rate=2, kernel_size=(3, 3), activation='relu', normalization=None, dropout=0.2, augment_factor=5):
     
     ##### load data and optional test data #####
     images, masks = load_data(data_path, imgs, msks, x_size, y_size)
     
     ##### save arguments for the model to dictionairy #####
-    arg_dict_model = {"img_shape":(x_size, y_size, 1), "start_ch": start_ch, "depth": depth, "inc_rate": inc_rate, "kernel_size": kernel_size, "activation": activation, "normalization": normalization, "dropout": dropout, "learning_rate": learning_rate, "up": up}
+    arg_dict_model = {"img_shape":(x_size, y_size, 1), "start_ch": start_ch, "depth": depth, "inc_rate": inc_rate, "kernel_size": kernel_size, "activation": activation, "normalization": eval(str(normalization)), "dropout": dropout, "learning_rate": learning_rate, "up": up}
     
     ##### prepare for k-fold cross validation #####
     kfold = KFold(n_splits=num_folds, shuffle=True)
@@ -52,7 +50,7 @@ def train_model(data_path=data_path, imgs="mr_bffe.mhd", msks="prostaat.mhd", mo
         callbacks_list = load_callback_list(save_dir)
        
         ##### fit model #####
-        arg_dict_fit = {"x": train_gen, "validation_data": val_gen, "epochs": nr_epochs, "verbose": verbosity, "callbacks": callbacks_list, "steps_per_epoch": (len(train_im)*augment_factor) // batch_size}
+        arg_dict_fit = {"x": train_gen, "validation_data": val_gen, "epochs": nr_epochs, "verbose": verbosity, "callbacks": callbacks_list, "steps_per_epoch": (len(train_im)*augment_factor) // batch_size, "validation_steps": len(val_im)//batch_size}
         start_time = time()
         model.fit(**arg_dict_fit)
         train_time = int(time()-start_time)
